a61febe6ba3af53b36ce421f5de71523bf0f13ba
Ignacio <ignaciogarciareyna@live.com>
2022-11-03 01:39:48 -0300

    Se agregó el llamado a la API y las visuales

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import YouTube from "react-youtube";
 
 function App() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "77ac9b9acb030fb65e067b31a773b067";
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+
+  //Variables de estado
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
+
+  //Petición a la API
+  const fetchMovies = async (searchKey) => {
+    //Si no hay busqueda, que muestre "descubrir"
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+
+    setMovies(results);
+    setMovie(results[0]);
+  };
+
+  //Busqueda de películas
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
-    <h1>Películas</h1>
+    <div>
+      <h2 className="text-center mt-5 mb-5">Trailer Movies</h2>
+      {/* Buscador */}
+      <form className="container mb-4" onSubmit={searchMovies}>
+        <input
+          type="text"
+          placeholder="search"
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <button className="btn btn-primary">Search</button>
+      </form>
+      {/* Contenedor de poster de peliculas actuales */}
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div key={movie.id} className="col-md-4 mb-3">
+              {/* Debo agregar un onclick */}
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
