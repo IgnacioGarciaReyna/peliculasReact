dabf982d4abc7962240817258bf69aa1f5364865
Ignacio <ignaciogarciareyna@live.com>
2022-11-15 00:37:23 -0300

    Se agregó seleccionar pelicula

--- src/App.js
+++ src/App.js
@@ -30,6 +30,36 @@ function App() {
 
     setMovies(results);
     setMovie(results[0]);
+
+    //Mostrar algo por defecto para evitar un error
+    if (results.length) {
+      await fetchMovie(results[0].id);
+    }
+  };
+
+  //Petición de un solo objeto para mostrar en reproductor de video
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "video",
+      },
+    });
+
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        // cuando encontremos un campo que se llame official trailer lo guarde
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+    setMovie(data);
+  };
+
+  const selectMovie = async (movie) => {
+    fetchMovie(movie.id);
+    setMovie(movie);
+    window.scrollTo(0, 0);
   };
 
   //Busqueda de películas
@@ -54,12 +84,74 @@ function App() {
         />
         <button className="btn btn-primary">Search</button>
       </form>
+      {/* Contenedor del banner y reproductor de video*/}
+      <div>
+        <main>
+          {movie ? (
+            <div
+              className="viewtrailer"
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
+                <>
+                  <YouTube
+                    videoId={trailer.key}
+                    className="reproductor container"
+                    containerClassName={"youtube-container"}
+                    opts={{
+                      // Tomado de youtube react
+                      with: "100%",
+                      height: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 0,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      },
+                    }}
+                  />
+                  <button onClick={() => setPlaying(false)} className="boton">
+                    Close
+                  </button>
+                </>
+              ) : (
+                <div className="container">
+                  <div>
+                    {trailer ? (
+                      <button
+                        className="boton"
+                        onClick={() => setPlaying(true)}
+                        type="button"
+                      >
+                        Play Trailer
+                      </button>
+                    ) : (
+                      "Sorry, no trailer available"
+                    )}
+                    <h1 className="text-white">{movie.title}</h1>
+                    <p className="text-white">{movie.overview}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+        </main>
+      </div>
       {/* Contenedor de poster de peliculas actuales */}
       <div className="container mt-3">
         <div className="row">
           {movies.map((movie) => (
-            <div key={movie.id} className="col-md-4 mb-3">
-              {/* Debo agregar un onclick */}
+            <div
+              key={movie.id}
+              className="col-md-4 mb-3"
+              onClick={() => selectMovie(movie)}
+            >
               <img
                 src={`${URL_IMAGE + movie.poster_path}`}
                 alt=""
