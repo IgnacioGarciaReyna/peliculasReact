2c7f0608bda9543926859318ce62369d9b0a5903
Ignacio <ignaciogarciareyna@live.com>
2022-12-17 15:53:11 -0300

    Se agregó foto de cada actor

--- src/components/Cast.js
+++ src/components/Cast.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Cast = ({ cast }) => {
+  let principalCast = [];
+  const IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+  for (let i = 0; i < 5; i++) {
+    principalCast.push(cast[i]);
+  }
+
   return (
     <div>
-      {cast[0] ? cast.map((actor) => <p key={actor.id}>{actor.name}</p>) : null}
+      {principalCast[0]
+        ? principalCast.map((actor) => (
+            <div>
+              <img width="80px" src={`${IMG_URL}${actor.profile_path}`} alt={actor.name} />
+              <p className="text-white" key={actor.id}>{actor.name}</p>
+            </div>
+          ))
+        : null}
     </div>
   );
 };
