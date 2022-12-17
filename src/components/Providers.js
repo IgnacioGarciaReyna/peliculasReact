90a3b80bbf8047c4669dbf6e08f6accf52eabe45
Ignacio <ignaciogarciareyna@live.com>
2022-12-17 01:37:16 -0300

    Se creó el componente Providers

---
+++ src/components/Providers.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Providers = ({ providers }) => {
+  return (
+    <div>
+      {providers.AR
+        ? providers.AR.rent.map((provider) => (
+            <p key={provider.id} className="text-white">
+              {provider.provider_name}
+            </p>
+          ))
+        : null}
+    </div>
+  );
+};
+
+export default Providers;
