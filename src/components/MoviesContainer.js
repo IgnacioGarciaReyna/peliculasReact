f3ca0361bcfbbba9de678b25487b41ffc16c3adc
Ignacio <ignaciogarciareyna@live.com>
2023-02-15 17:58:56 -0300

    Swiper was added to moviesContainer

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -1,20 +1,42 @@
 import React, { Fragment } from "react";
 import MovieCard from "./MovieCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+// import required modules
+import { Pagination } from "swiper";
 
 const MoviesContainer = ({ movies, URL_IMAGE }) => {
   return (
     <Fragment>
-      <div className="container-movies">
+      {/* <div className="container-movies">
         <div className="row-movies">
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
-              URL_IMAGE={URL_IMAGE}
-            />
-          ))}
+          <div className="wrapper">
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} URL_IMAGE={URL_IMAGE} />
+            ))}
+          </div>
         </div>
-      </div>
+      </div> */}
+      <Swiper
+        slidesPerView={7}
+        spaceBetween={0}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {movies.map((movie) => (
+          <SwiperSlide>
+            <MovieCard key={movie.id} movie={movie} URL_IMAGE={URL_IMAGE} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Fragment>
   );
 };
