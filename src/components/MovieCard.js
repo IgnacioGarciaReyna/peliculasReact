741489a5dfd9f7720f6332ed25806fec45fe12db
Ignacio <ignaciogarciareyna@live.com>
2023-02-15 19:59:30 -0300

    Title was added to series cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie, URL_IMAGE }) => {
   return (
-    <Link
-      to={`/movie/${movie.id}`}
-      className=""
-    >
+    <Link to={`/movie/${movie.id}`} className="">
       <div className="container-movie-card" key={movie.id}>
         <img className="img-movie" src={`${URL_IMAGE + movie.poster_path}`} />
-        <p className="title-movie-card">{movie.title}</p>
+        <p className="title-movie-card">
+          {movie.title ? movie.title : movie.name}
+        </p>
       </div>
     </Link>
   );
