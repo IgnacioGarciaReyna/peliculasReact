778dccd13fee45422dbde94e6064fbc48e4cf294
Ignacio <ignaciogarciareyna@live.com>
2023-02-16 16:58:30 -0300

    Nav link to home was added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import Search from "./Search";
 
 const Nav = ({ fetchMovies, searchType }) => {
   return (
     <nav className="navigation-container">
-      <h1 className="page-title">Black Lodge Trailers</h1>
+      <Link to={`/`}>
+        <h1 className="page-title">Black Lodge Trailers</h1>
+      </Link>
       <Search fetchMovies={fetchMovies} searchType={searchType} />
     </nav>
   );
