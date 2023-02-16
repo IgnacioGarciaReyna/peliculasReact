029820256116ed1ddadd725339f687abb2bd8613
Ignacio <ignaciogarciareyna@live.com>
2023-02-16 18:44:30 -0300

    Swiper was added to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,61 @@
 import React, { useState } from "react";
 import Stars from "./Stars";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 const Home = ({ movies, IMAGE_PATH }) => {
-  console.log(movies);
   return (
     <div className="home-container">
-      {movies[0] ? (
-        <div
-          className="movie-page-container"
-          style={{
-            backgroundImage: `url("${IMAGE_PATH}${movies[0].backdrop_path}")`,
-          }}
-        >
-          <div className="background-cover">
-            <p>{movies[0].release_date}</p>
-            <p className="home-movie-title">{movies[0].title} </p>
-            <img
-              className="home-movie-poster"
-              src={`${IMAGE_PATH}${movies[0].poster_path}`}
-              alt=""
-            />
-            <Stars vote_average={movies[0].vote_average} />
-            <div className="overview-container">
-              <p className="home-overview">{movies[0].overview}</p>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {movies.map((movie) => (
+          <SwiperSlide>
+            <div
+              className="movie-page-container"
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              <div className="background-cover">
+                <p>{movie.release_date}</p>
+                <p className="home-movie-title">{movie.title} </p>
+                <img
+                  className="home-movie-poster"
+                  src={`${IMAGE_PATH}${movie.poster_path}`}
+                  alt=""
+                />
+                <Stars vote_average={movie.vote_average} />
+                <div className="overview-container">
+                  <p className="home-overview">{movie.overview}</p>
+                </div>
+                <div>
+                  <button>Ver ficha</button>
+                  <button>Trailer</button>
+                </div>
+              </div>
             </div>
-            <div>
-              <button>Ver ficha</button>
-              <button>Trailer</button>
-            </div>
-          </div>
-        </div>
-      ) : null}
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
