0c07eea742764c9147af77276b3d5d70254c3b8b
Ignacio <ignaciogarciareyna@live.com>
2023-02-16 19:58:38 -0300

    Null trailer issue was solved

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -4,29 +4,31 @@ import YouTube from "react-youtube";
 const Trailer = ({ trailer }) => {
   return (
     <Fragment>
-      <p>{trailer.type}</p>
-      <p>{trailer.name}</p>
       {trailer ? (
-        <YouTube
-          videoId={trailer.key}
-          className="trailer-reproductor"
-          containerClassName={"youtube-container"}
-          opts={{
-            // Tomado de youtube react
-            width: "600px",
-            height: "100%",
-            playerVars: {
-              autoplay: 0,
-              controls: 0,
-              cc_load_policy: 0,
-              fs: 0,
-              iv_load_policy: 0,
-              modestbranding: 0,
-              rel: 0,
-              showinfo: 0,
-            },
-          }}
-        />
+        <Fragment>
+          <p>{trailer.type}</p>
+          <p>{trailer.name}</p>
+          <YouTube
+            videoId={trailer.key}
+            className="trailer-reproductor"
+            containerClassName={"youtube-container"}
+            opts={{
+              // Tomado de youtube react
+              width: "600px",
+              height: "100%",
+              playerVars: {
+                autoplay: 0,
+                controls: 0,
+                cc_load_policy: 0,
+                fs: 0,
+                iv_load_policy: 0,
+                modestbranding: 0,
+                rel: 0,
+                showinfo: 0,
+              },
+            }}
+          />
+        </Fragment>
       ) : null}
     </Fragment>
   );
