4955d952ded75fa25e0ebebac088abb480234277
Ignacio <ignaciogarciareyna@live.com>
2023-02-25 17:23:46 -0300

    Merge problem was solved

--- src/components/Home.js
+++ src/components/Home.js
@@ -69,7 +69,6 @@ const Home = ({ IMAGE_PATH }) => {
               }}
             >
               <div className="background-cover-container">
-                <p className="movie-date home-movie-release-date">{movie.release_date}</p> 
                 <p className="movie-date home-movie-release-date">{movie.release_date}</p>
                 <p className="home-movie-title">{movie.title} </p>
                 <Stars vote_average={movie.vote_average} />
