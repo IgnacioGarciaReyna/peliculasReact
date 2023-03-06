dee6e1b19053146d4a106a3656d01fb56b2f72a1
Ignacio <ignaciogarciareyna@live.com>
2023-03-06 16:00:54 -0300

    Animation was added to movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import emptyPoster from "./../assets/img/istock-photo.jpg";
+//Aos imports
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const MovieCard = ({ movie }) => {
   const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <Link
       to={movie.title ? `/movie/${movie.id}` : `/tv/${movie.id}`}
       className=""
     >
-      <div className="container-movie-card" key={movie.id}>
+      <div
+        className="container-movie-card"
+        key={movie.id}
+        data-aos="flip-right"
+      >
         <div className="img-movie-container">
           <img
             className="img-movie"
