34353e0a1df54d64a017d05536d33efd1045e8a9
Ignacio <ignaciogarciareyna@live.com>
2023-03-06 20:24:14 -0300

    Nav animation was added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import Search from "./Search";
+//AOS Animations
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
 
 const Nav = ({ fetchMovies, searchType }) => {
   return (
-    <nav className="navigation-container">
+    <nav className="navigation-container" data-aos="fade-down">
       <Link to={`/`}>
         <h1 className="page-title">Cinema Paradiso Trailers</h1>
       </Link>
