ed78f402e4a9655740870d37b57dfde1a3fe0376
Ignacio <ignaciogarciareyna@live.com>
2023-03-06 20:56:36 -0300

    Scroll problem was solved

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,20 +2,16 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import emptyPoster from "./../assets/img/istock-photo.jpg";
 
-
 const MovieCard = ({ movie }) => {
   const URL_IMAGE = "https://image.tmdb.org/t/p/original";
-
 
   return (
     <Link
       to={movie.title ? `/movie/${movie.id}` : `/tv/${movie.id}`}
+      onClick={window.scrollTo(0, 0)}
       className=""
     >
-      <div
-        className="container-movie-card"
-        key={movie.id}
-      >
+      <div className="container-movie-card" key={movie.id}>
         <div className="img-movie-container">
           <img
             className="img-movie"
