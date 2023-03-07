c52cadd33ad35f6944d045c465577e9d2d3cf957
Ignacio <ignaciogarciareyna@live.com>
2023-03-06 21:09:10 -0300

    Search spinner was added

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { BounceLoader } from "react-spinners";
 import Footer from "./Footer";
-import MovieCard from "./MovieCard";
 import MoviesContainer from "./MoviesContainer";
 
 const SearchResults = ({}) => {
+  const [spinner, setSpinner] = useState(true);
   const { key } = useParams();
   //Strings para el fetch de varias Movies
-
   const searchMoviesType = "/search/movie";
   const searchSeriesType = "/search/tv";
-
+  setTimeout(() => setSpinner(false), 800);
   return (
     <div className="home-container">
+      {spinner ? (
+        <div className="spinner-container">
+          <BounceLoader color="#007aff" />
+        </div>
+      ) : null}
       <MoviesContainer
         title={`Results for "${key}" in films`}
         moviesType={searchMoviesType}
